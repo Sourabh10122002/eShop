ea124439633ff685e593fdf09f4beaaa172ef329
Sourabh10122002 <souravrawat444@gmail.com>
2024-07-03 02:19:26 +0530

    frontend check

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade"> Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptatem quos perferendis itaque voluptas! Ipsa beatae suscipit sequi ut tempora in nobis atque. Architecto nihil, esse tempora aliquid iusto consequuntur rem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptatem quos perferendis itaque voluptas! Ipsa beatae suscipit sequi ut tempora in nobis atque. Architecto nihil, esse tempora aliquid iusto consequuntur.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel beatae natus labore perspiciatis esse, fuga culpa dolores? Ut adipisci mollitia soluta, pariatur nisi animi debitis! Nemo vel cumque sit consectetur.
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Item.css'
+import { Link } from 'react-router-dom'
+
+const Item = (props) => {
+    const { image, name, new_price, old_price } = props;
+    return (
+        <div className='item'>
+            <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0, 0)} src={image} alt="" /></Link>
+            <p>{name}</p>
+            <div className="item-prices">
+                <div className="item-price-new">
+                    ${new_price}
+                </div>
+                <div className="item-price-old">
+                    &{old_price}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item
